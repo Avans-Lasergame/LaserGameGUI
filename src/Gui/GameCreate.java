329202b415d0ac10d9eb31b3fda572d1e127cdd2
DaanF1 <daanstander@hotmail.nl>
2024-04-04 17:38:42 +0200

    Dynamic Player selection
    Added dynamic data for Players

--- src/Gui/GameCreate.java
+++ src/Gui/GameCreate.java
@@ -1,25 +1,31 @@
 package Gui;
 
 import Objects.*;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
-
+import javafx.util.StringConverter;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class GameCreate{
-    private static ArrayList<Player> players = GUI.getPlayers();
+    private static ArrayList<Player> players = GUI.getPlayers(); //All Players
+    private static ObservableList<Player> selectedPlayers = FXCollections.observableArrayList();  //Selected Players for a game
+    private static int maximunPlayerCount = 4;
+
     public static VBox getComponent() {
+        // General settings
         VBox gameCreateBox = new VBox(20);
         gameCreateBox.setPadding(new Insets(20));
 
-        //#region Select gamemode
+        //#region Select GameMode ComboBox
+        Label labelSelectGameType = new Label("Select game type:");
         ComboBox selectGameType = new ComboBox();
+        selectGameType.setPrefWidth(200);
         List<String> selectionGameModes = new LinkedList<>();
         for (GameModes value : GameModes.values()){
             selectionGameModes.add(String.valueOf(value));
@@ -29,39 +35,101 @@ public class GameCreate{
         selectGameType.getSelectionModel().selectFirst();
         //#endregion
 
-        //#region Select player
+        //#region Select Player ComboBox
+        Label labelSelectPlayer = new Label("Select Players:");
         ComboBox selectPlayer = new ComboBox();
-        List<String> playerNames = new LinkedList<>();
-        for (Player player : players){
-            playerNames.add(player.getName());
-        }
-        ObservableList<String> playerList = FXCollections.observableList(playerNames);
+        selectPlayer.setPrefWidth(200);
+        ObservableList<Player> playerList = FXCollections.observableArrayList(players);
         selectPlayer.setItems(playerList);
+        selectPlayer.setConverter(new StringConverter<Player>() {
+            @Override
+            public String toString(Player player){
+                return player.getName();
+            }
+            @Override
+            public Player fromString(String string) {
+                return (Player) selectPlayer.getItems().stream().filter(player ->
+                        player.equals(string)).findFirst().orElse(null);
+            }
+        });
         selectPlayer.getSelectionModel().selectFirst();
         //#endregion
 
-        //#region List of players
+        //#region List of players TableView
         TableView<Player> playerTable = new TableView<>();
-        if (!players.isEmpty()){
-            //ObservableList<Player> playerList = FXCollections.observableList(players);
-            //playerTable.setItems(playerList);
-
-            TableColumn playerColumn = new TableColumn("Players");
-            playerColumn.setCellValueFactory(new PropertyValueFactory<>("player"));
-        }
-//        TableView playerTable = new TableView();
-//        TableColumn<Player, Object> allPlayerColumn = new TableColumn<>("All Players");
-//        //TableColumn<Player, Object> selectedPlayerColumn = new TableColumn<>("Selected Players");
-//        playerTable.getColumns().addAll(allPlayerColumn);
-//
-//        if (!players.isEmpty()){
-//            ObservableList<Player> playerList = FXCollections.observableList(players);
-//            playerTable.setItems(playerList);
-//        }
+        playerTable.setPrefWidth(200);
+        TableColumn<Player, String> playerColumn = new TableColumn("Selected Players");
+        playerColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getName()));
+        playerColumn.setPrefWidth(playerTable.getPrefWidth());
+        playerTable.getColumns().add(playerColumn);
+        playerTable.setItems(selectedPlayers);
         //#endregion
 
 
-        gameCreateBox.getChildren().addAll(selectGameType, selectPlayer, playerTable);
+        // Action items:
+        //#region Add Player Button
+        Button buttonAddPlayer = new Button("Add Player to List");
+        buttonAddPlayer.setOnAction(e -> {
+            Object selectedGameMode = selectGameType.getSelectionModel().getSelectedItem();
+            if (selectedGameMode == String.valueOf(GameModes.FreeForAll)){
+                if (selectedPlayers.size() < maximunPlayerCount){
+                    Player selectedPlayer = (Player) selectPlayer.getSelectionModel().getSelectedItem();
+                    selectedPlayers.add(selectedPlayer);
+                    // Show alert
+                    Alert addedPlayer = new Alert(Alert.AlertType.INFORMATION);
+                    addedPlayer.setHeaderText("Success!");
+                    addedPlayer.setContentText(selectedPlayer.getName() + " was added to the list of selected players!");
+                    addedPlayer.showAndWait();
+                } else{
+                    // Show alert
+                    Alert addedPlayer = new Alert(Alert.AlertType.INFORMATION);
+                    addedPlayer.setHeaderText("Error!");
+                    addedPlayer.setContentText("Maximum amount of Players reached!");
+                    addedPlayer.showAndWait();
+                }
+            } else {
+                selectedPlayers.clear();
+                // Show alert
+                Alert addedPlayer = new Alert(Alert.AlertType.INFORMATION);
+                addedPlayer.setHeaderText("Error!");
+                addedPlayer.setContentText("You must change the GameMode to add individual Players!");
+                addedPlayer.showAndWait();
+            }
+        });
+        //#endregion
+
+        //#region Remove Player Button
+        Button buttonRemovePlayer = new Button("Remove last Player from List");
+        buttonRemovePlayer.setOnAction(e -> {
+            Object selectedGameMode = selectGameType.getSelectionModel().getSelectedItem();
+            if (selectedGameMode == String.valueOf(GameModes.FreeForAll)){
+                if (!selectedPlayers.isEmpty()){
+                    selectedPlayers.remove(selectedPlayers.size() - 1);
+                    // Show alert
+                    Alert addedPlayer = new Alert(Alert.AlertType.INFORMATION);
+                    addedPlayer.setHeaderText("Success!");
+                    addedPlayer.setContentText("The last Player was removed from the list of selected players!");
+                    addedPlayer.showAndWait();
+                } else{
+                    // Show alert
+                    Alert addedPlayer = new Alert(Alert.AlertType.INFORMATION);
+                    addedPlayer.setHeaderText("Error!");
+                    addedPlayer.setContentText("There are no Players found in the list!");
+                    addedPlayer.showAndWait();
+                }
+            } else {
+                selectedPlayers.clear();
+                // Show alert
+                Alert addedPlayer = new Alert(Alert.AlertType.INFORMATION);
+                addedPlayer.setHeaderText("Error!");
+                addedPlayer.setContentText("You must change the GameMode to add individual Players!");
+                addedPlayer.showAndWait();
+            }
+        });
+        //#endregion
+
+        gameCreateBox.getChildren().addAll(labelSelectGameType, selectGameType, labelSelectPlayer, selectPlayer,
+                buttonAddPlayer, playerTable, buttonRemovePlayer);
         return gameCreateBox;
     }
 }
