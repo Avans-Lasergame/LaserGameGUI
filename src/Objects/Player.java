367370930ecddff37358fcf84426ee2fa2b07e76
S.k2004 <79660690+s-kroonen@users.noreply.github.com>
2024-04-10 19:43:39 +0200

    removed old files

--- src/Objects/Player.java
+++ src/Objects/Player.java
@@ -1,33 +1,5 @@
 package Objects;
 
 public class Player{
-    private Hub hub;
-    private String playerName;
-    private int health;
-
-    public Player(Hub hub, String playerName){
-        this.hub = hub;
-        this.playerName = playerName;
-        this.health = 100;
-    }
-
-    // Get items
-    public Hub getPlayerHub(){
-        return this.hub;
-    }
-    public String getPlayerName(){
-        return this.playerName;
-    }
-    public int getPlayerHealth(){
-        return this.health;
-    }
-
-    // Set items
-    public int reduceHealth(){
-        if (this.hub.getVest().isHit()){
-            this.health = this.health-10;
-        }
-        return this.health;
-    }
 
 }
